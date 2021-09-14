390278a82f464a3e7a7d5b832e2c396b4d79058e
saepudinjeh <saepudinjeh@gmail.com>
2021-09-14 16:35:12 +0700

    add route and views

--- src/scripts/views/pages/frontPage.js
+++ src/scripts/views/pages/frontPage.js
@@ -1,7 +1,34 @@
+/* eslint-disable max-len */
 const FrontPage = {
   async render() {
     return `
-      <h1>Ini Front Page</h1>  
+      <section class="w-full h-screen">
+        <div class="flex flex-col">
+          <div class="mx-auto">
+            <img class="w-44 h-44 mx-auto rounded-full object-cover" src="https://images.unsplash.com/photo-1491841550275-ad7854e35ca6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80" alt="profile">
+          </div>
+
+          <div class="mx-auto mt-6 text-center text-sm">
+            <h1 class="text-xl font-medium">Agung Saepudin</h1>
+            <p class="text-center mt-3">
+              Hi, I'm Agung Saepudin, a 
+              <span class="font-bold">Fullstack Developer.</span>
+            </p>
+            <p>
+              For the Front end i usually use the 
+              <span class="font-bold">Vue JS</span>
+            </p>
+            <p>
+              while for the Backend i usually use
+            </p>
+            <p class="font-bold">Express JS</p>
+
+            <button class="mt-6 px-5 py-2 rounded-lg text-base shadow text-white bg-gradient-to-b from-blue-400 to-purple-500 hover:form-blue-500 hover:to-purple-600 font-medium">
+              My Resume
+            </button>
+          </div>
+        </div>
+      </section>  
     `;
   },
 
--- src/scripts/views/pages/galeryPage.js
+++ src/scripts/views/pages/galeryPage.js
@@ -1,7 +1,30 @@
 const GaleryPage = {
   async render() {
     return `
-      <h1>Ini Galery Page</h1>  
+    <section class="flex justify-center px-6 mt-5 mx-auto w-full">
+      <div class="w-full">
+        <div class="divide-y divide-solid divide-blue-400">
+          <h1 class="flex justify-center text-xl font-medium border-blue-400 cursor-pointer">
+            My Galery
+          </h1>
+        </div>
+
+        <div class="mt-10 grid md:grid-cols-3 grid-cols-1 gap-2 w-full">
+
+          <div class="w-full bg-blue-300">
+            <img class="w-full h-60 rounded-lg object-cover" src="https://images.unsplash.com/photo-1521123946922-eb346044f2ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=623&q=80" alt="">
+          </div>
+
+          <div class="w-full bg-blue-300">
+            <img class="w-full h-60 rounded-lg object-cover" src="https://images.unsplash.com/photo-1521123946922-eb346044f2ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=623&q=80" alt="">
+          </div>
+
+          <div class="w-full bg-blue-300">
+            <img class="w-full h-60 rounded-lg object-cover" src="https://images.unsplash.com/photo-1521123946922-eb346044f2ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=623&q=80" alt="">
+          </div>
+        </div>
+      </div>
+    </section>   
     `;
   },
 
